b6f39662b75a5b37da1dd1512f15f91baa22e4e4
db30048883 <dylan.beavers@outlook.com>
2026-02-27 10:34:11 -0900

    Build offline rock entry tracker with photos

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,38 @@
-self.addEventListener('install', function(e) {
-  e.waitUntil(
-    caches.open('rockweigher-store').then(function(cache) {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/manifest.json'
-      ]);
-    })
+const CACHE_NAME = 'rock-tracker-v2';
+const APP_ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './service-worker.js'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_ASSETS))
   );
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', function(e) {
-  e.respondWith(
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request);
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) return cachedResponse;
+      return fetch(event.request).then((networkResponse) => {
+        const copy = networkResponse.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+        return networkResponse;
+      }).catch(() => caches.match('./index.html'));
     })
   );
 });
